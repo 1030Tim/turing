74c736ca2e1594474959700bcc4b93ee0d801741
Turing <jun20061030@gmail.com>
2026-07-23 12:39:29 +0800

    fix mongodb connection

--- database.py
+++ database.py
@@ -1,36 +1,38 @@
 from pymongo import MongoClient
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
 
-# 讀取 .env
 load_dotenv()
 
 
-# 連線 MongoDB Atlas
-client = MongoClient(
-    os.getenv("MONGO_URI")
-)
+MONGO_URI = os.getenv("MONGO_URI")
 
 
-# 建立 database
-db = client["JunOS"]
-
-
-# 建立 collection
-records = db["records"]
-
-
-
-def init_db():
-
-    # MongoDB 不需要 CREATE TABLE
-    # 第一次 insert 時會自動建立 collection
-
-    return True
+client = None
 
 
 
 def get_db():
 
-    return records
+    global client
+
+
+    if client is None:
+
+        client = MongoClient(
+            MONGO_URI
+        )
+
+
+    db = client["junos"]
+
+
+    return db["records"]
+
+
+
+
+def init_db():
+
+    print("MongoDB connected")
